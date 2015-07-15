33be505421e2af26f5f04ff2c9d8cb997cb2af53
Andrew <Muntyan>
2015-07-15 14:58:34 +0300

    Dashboard screen #4

--- app/components/dashboard.component.jsx
+++ app/components/dashboard.component.jsx
@@ -3,29 +3,71 @@
 import React, {
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  ListView,
+  UIExplorerPage,
+  Image,
+  TouchableHighlight
+  } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Colors from './jss/colors-scheme';
 import SettingsStore from 'stores/settings.store';
-import NotLoggenInComponent from './shared/not-logged-in.component';
+import NotLoggedInComponent from './utils/not-logged-in.component';
+import JobsComponent from './jobs.component';
+//import ApplicationComponent from './application.component';
+//import EnvironmentComponent from './environment.component';
+//import PipelinesComponent from './pipelines.component';
 
 
 var styles = StyleSheet.create({
   tabContent: {
-    flex: 1,
-    alignItems: 'center'
+    flex: 1
   },
-  tabText: {
-    color: 'black',
-    margin: 50
+  row: {
+    flexDirection: 'row',
+    padding: 10,
+    backgroundColor: Colors.gray
+  },
+  icon: {
+    marginRight: 10,
+    color: '#000'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCCCCC'
+  },
+  text: {
+    flex: 1,
+    fontSize: 25
   }
 });
+
+var listItems = [
+  {
+    name: 'Applications',
+    icon: 'toggle-right',
+    targetComponent: JobsComponent,
+    targetComponentTitle: 'Jobs list'
+  },
+  {
+    name: 'Environments',
+    icon: 'server',
+    targetComponent: JobsComponent,
+    targetComponentTitle: 'Jobs list'
+  },
+  {
+    name: 'Pipelines',
+    icon: 'pied-piper',
+    targetComponent: JobsComponent,
+    targetComponentTitle: 'Jobs list'
+  }
+];
 
 function Refresh (smartLoad=false) {
   if (!SettingsStore.getState().user) {
     return;
   }
-
 }
 
 export default React.createClass({
@@ -41,14 +83,15 @@ export default React.createClass({
   },
 
   getInitialState() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     return {
-      settings: SettingsStore.getState()
+      settings: SettingsStore.getState(),
+      dataSource: ds.cloneWithRows(listItems)
     };
   },
 
   componentDidMount() {
     SettingsStore.on('change', this.handleChange);
-    //JobsActions.getJobs();
   },
 
   componentWillUnmount() {
@@ -60,7 +103,7 @@ export default React.createClass({
       settings: SettingsStore.getState()
     });
   },
-  
+
   _handleBackButtonPress() {
     console.log('back');
     this.props.navigator.pop();
@@ -71,17 +114,44 @@ export default React.createClass({
     this.props.navigator.push(nextRoute);
   },
 
-  render() {
-    console.log(this.state);
+  _renderRow: function(rowData, sectionID, rowID) {
+    return (
+      <TouchableHighlight
+        onPress={
+          this.goToNextScreen.bind(rowData.targetComponent, rowData.targetComponentTitle)
+          }
+        //Change this color to darker one
+        underlayColor={Colors.gray}
+      >
+        <View>
+          <View style={styles.row}>
+            <Icon name={rowData.icon} style={styles.icon} size={30}/>
+            <Text style={styles.text}>{rowData.name}</Text>
+          </View>
+          <View style={styles.separator} />
+        </View>
+      </TouchableHighlight>
+    );
+  },
 
+  goToNextScreen: function(targetComponent, targetComponentTitle) {
+    this.props.navigator.push({
+      component: targetComponent,
+      title: targetComponentTitle
+    });
+  },
+
+  render() {
     if (!this.state.settings.user) {
-      return (<NotLoggenInComponent />);  
+      return (<NotLoggedInComponent />);
     }
 
     return (
       <View style={[styles.tabContent, {backgroundColor: '#FFF'}]}>
-        <Text style={styles.tabText}>{'Dashboard'}</Text>
-        <Text style={styles.tabText}>{'Details'}</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderRow}
+        />
       </View>
     );
   }
